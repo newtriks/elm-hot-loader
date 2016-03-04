e24898dd54d9fdc1ff97e946b4c707ab09caa2b8
Simon Bailey <simon@newtriks.com>
2016-03-04 11:23:37 +0000

    refactor: original project files
    Signed-off-by: Simon Bailey <simon@newtriks.com>

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,8 @@
 import Effects exposing (Never)
-import Counter
 import StartApp
 import Task
 
-import App exposing (..)
+import Components.App exposing (..)
 
 app : StartApp.App Model
 app =
@@ -17,10 +16,6 @@ app =
 main =
   app.html
 
-port tasks : Signal (Task.Task Never ())
-port tasks =
-  app.tasks
-
 -- HOT LOADER
 
 -- After hot swapping, elm-hot-load will call 'swap' port to trigger a re-render.
@@ -29,7 +24,3 @@ port swap : Signal Bool
 swapsignal : Signal Action
 swapsignal =
   Signal.map (\_ -> Empty) swap
-
--- Used by default example
-port logs : Signal String
-port logs = logMailbox.signal
--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
-'use strict';
-
-require('./index.html');
-require("./styles.scss");
-
-var Elm = require('./Main');
-
-var elm = Elm.fullscreen(Elm.Main, { swap: false });
-
-//interop
-elm.ports.logs.subscribe(function(log) {
-  var node = document.createElement('div');
-  node.style.color = 'red';
-  node.innerHTML = 'JS log: ' + log + ', time = ' + Date.now();
-  document.body.appendChild(node);
-});
+import 'babel-polyfill';
+import './index.html';
+import './css/App.css';
+import * as Elm from './Main';
+const elm = Elm.fullscreen(Elm.Main, { swap: false });
